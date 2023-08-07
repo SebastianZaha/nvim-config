af3d4133c5cc5611267f1309a5ccac877d267f91
Sebastian Zaha <sebastian.zaha@gmail.com>
2023-08-07 22:55:13 +0200

    Alphabetize bindings and add colors telescope bind

--- lua/custom/bindings.lua
+++ lua/custom/bindings.lua
@@ -43,18 +43,19 @@ vim.keymap.set('n', '<leader>E', ':Neotree reveal<cr>')
 local builtin = require('telescope.builtin')
 local themes  = require('telescope.themes')
 
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>s/',  builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[Search] existing [B]uffers' })
-vim.keymap.set('n', '<leader>s?', builtin.oldfiles, { desc = 'Find recently opened files' })
-vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>s;', builtin.command_history, { desc = '[S]earch Commands' })
 vim.keymap.set('n', '<leader>s.',
   function() builtin.current_buffer_fuzzy_find(themes.get_dropdown({previewer = false})) end,
   { desc = 'Fuzzily search in current buffer' })
+vim.keymap.set('n', '<leader>s/',  builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>s;', builtin.command_history, { desc = '[S]earch Commands' })
+vim.keymap.set('n', '<leader>s?', builtin.oldfiles, { desc = 'Find recently opened files' })
+vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[Search] existing [B]uffers' })
+vim.keymap.set('n', '<leader>sc', builtin.colorscheme, { desc = 'Colorschemes' })
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 
 -- gitsigns [H]unk bindings
 require('gitsigns').setup{
